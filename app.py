e12ff1cebd0fca56cd98b7f4ee08567fa4ab3cc4
Zhenya <57045129+Zhenya1975@users.noreply.github.com>
2022-03-01 11:56:15 +0000

    первичная разметка

--- app.py
+++ app.py
@@ -1,1 +1,131 @@
-print("hello")
+import pandas as pd
+# import numpy as np
+from dash import Dash, dcc, html, Input, Output, callback_context, State, callback_context
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import ThemeSwitchAIO
+from dash_bootstrap_templates import load_figure_template
+import datetime
+
+import tab_main
+# import tab_coverage
+# import tab_settings
+
+
+
+# import functions
+# import title_text
+# import fig_downtime_by_years
+# import table_maintanance_xlsx
+# import fig_ktg_by_years
+# import fig_planned_3y_ktg
+# import fig_piechart_downtime_by_categories
+# import ktg_by_month_models
+# import ktg_table_html
+
+
+
+# import initial_values
+
+from dash import dash_table
+import base64
+import io
+import json
+import plotly.graph_objects as go
+# import fig_coverage
+
+# select the Bootstrap stylesheet2 and figure template2 for the theme toggle here:
+# template_theme1 = "sketchy"
+template_theme1 = "flatly"
+template_theme2 = "darkly"
+# url_theme1 = dbc.themes.SKETCHY
+url_theme1 = dbc.themes.FLATLY
+url_theme2 = dbc.themes.DARKLY
+
+
+
+loading_style = {
+    # 'position': 'absolute',
+                 # 'align-self': 'center'
+                 }
+
+templates = [
+    "bootstrap",
+    "minty",
+    "pulse",
+    "flatly",
+    "quartz",
+    "cyborg",
+    "darkly",
+    "vapor",
+    "sandstone"
+]
+
+load_figure_template(templates)
+
+dbc_css = (
+    "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.1/dbc.min.css"
+)
+app = Dash(__name__, external_stylesheets=[url_theme1, dbc_css])
+
+"""
+===============================================================================
+Layout
+"""
+
+tabs_styles = {
+    'height': '34px'
+}
+
+
+
+app.layout = dbc.Container(
+    dbc.Row(        [
+            dbc.Col(
+                [
+                    html.H4("КТГ 2023-2025"),
+                    ThemeSwitchAIO(
+                      aio_id="theme", themes=[url_theme1, url_theme2],
+                    ),
+
+                    html.Div([
+                        dcc.Tabs(
+                            id="tabs-all",
+                            style={
+                                # 'width': '50%',
+                                # 'font-size': '200%',
+                                'height':'10vh'
+                            },
+                            value='ktg',
+                            # parent_className='custom-tabs',
+                            # className='custom-tabs-container',
+                            children=[
+                                tab_main.maintanance_chart_tab(),
+                                # coverage_tab.coverage_tab(),
+                                # messages_orders_tab.messages_orders_tab(),
+                                # orders_moved_tab.orders_moved_tab(),
+                                # settings_tab.settings_tab()
+
+                                # tab2(),
+                                # tab3(),
+                            ]
+                        ),
+                    ]),
+
+                ]
+            )
+        ]
+    ),
+    className="m-4 dbc",
+    fluid=True,
+    
+)
+
+
+######################### ОСНОВНОЙ ОБРАБОТЧИК ДЛЯ ПОСТРОЕНИЯ ГРАФИКОВ ##############################
+
+
+
+
+if __name__ == "__main__":
+    # app.run_server(debug=True)
+    app.run_server(host='0.0.0.0', debug=True)
---
+++ tab_main.py
@@ -0,0 +1,215 @@
+from dash import dcc, html
+import dash_bootstrap_components as dbc
+loading_style = {
+    # 'position': 'absolute',
+    # 'align-self': 'center'
+                 }
+def maintanance_chart_tab():
+    maintanance_chart_tab_block = dcc.Tab(
+        label='КТГ',
+        value='ktg',
+        children=[
+           
+          dcc.Loading(id='loading', parent_style=loading_style),
+          dcc.Loading(id='loading1', parent_style=loading_style),
+            html.Div(
+              html.P(),
+            ),
+          
+            html.Div(
+            dbc.Row([
+                # колонка с фильтрами
+                dbc.Col(width=2,
+                  children=[
+                    
+                    html.P(),
+                    html.Div([
+                      "Бизнес единица",
+                      dcc.Dropdown(id="checklist_level_1", multi=True, optionHeight = 50),
+                  ]),
+
+
+                    html.P(),
+                    html.Div([
+                      "Техместо вышестоящее",
+                      dcc.Dropdown(id="checklist_main_eo_class", multi=True),
+                  ]),
+                    
+                  
+                  html.P(),
+                    html.Div([
+                      "ЕО",
+                      dcc.Dropdown(id="checklist_eo", multi=True, optionHeight = 50),
+                  ]),
+                    html.Hr(),
+                    html.P('Категории работ'),
+                    html.Div(style={'marginLeft': '3px'},
+                                     children=[
+                                         dbc.Button("Выбрать все", size="sm",
+                                                    id="select_all_maintanance_category_checklist",
+                                                    style={'marginBottom': '3px',
+                                                           'marginTop': '3px',
+                                                           'backgroundColor': '#232632'}
+                                                    ),
+                                         dbc.Button("Снять выбор", color="secondary",
+                                                    size="sm",
+                                                    style={'marginBottom': '3px',
+                                                           'marginTop': '3px',
+                                                           'backgroundColor': '#232632'},
+                                                    id="release_all_maintanance_category_checklist"),
+
+                                         html.P(),
+                                         dcc.Checklist(
+                                             id='maintanance_category_checklist',
+                                             # options=regions,
+                                             # value=regions_list,
+                                             labelStyle=dict(display='block')),
+                                         html.Hr(className="hr"),
+                                     ]
+                               ),
+                  ]
+                ),
+                dbc.Col(width=10,
+                    children=[
+                            dbc.Row([
+                              dbc.Col(
+                                children = [
+                                  html.H5(id = 'be_title_id'),
+                                  # html.P(),
+                                  html.H5(id = 'level_upper_title_id'),
+                                  html.H5(id = 'number_of_eo_title_id')
+
+                                ]
+                                
+                          )
+                        ]),
+                        dbc.Row([
+                              dbc.Col(width = 4,
+                                children = [
+                                  dbc.Card(
+                                    dbc.CardBody(
+                                        [
+                                            html.H4("2023 год", className="card-title"),
+                                            #html.H6("Card subtitle", className="card-subtitle"),
+                                            html.P(id = 'downtime_2023'),
+                                            html.P(id = 'cal_fond_2023'),
+
+                                           
+                                        ]
+                                    ),
+                                    style={"width": "18rem"},
+
+                                  )
+
+                                ],
+                 
+                              ),
+                              dbc.Col(width = 4,
+                              
+                              ),
+                              dbc.Col(width = 4,
+                              
+                              )
+                        ]),
+                      html.Hr(),
+                      ###################### ряд с кнопками выгрузки таблиц в эксель ####################
+                      html.Div(
+                        dbc.Row([
+                      
+                          dbc.Col(width=2,
+                                  children=[
+                                    html.Div([
+                                      dbc.Button("Выгрузить простои xlsx", id="btn_download_downtime_table", size="sm",
+                                                 style={'marginBottom': '3px',
+                                                        'marginTop': '3px',
+                                                        'backgroundColor': '#232632'},),
+                                      dcc.Download(id="download_excel_downtime_table")
+                                    ])
+                                  ]
+                                 )
+                        ])
+                      ),
+
+                      #######################################################
+                      html.Div(
+                        dbc.Row([
+                      
+                          dbc.Col(width=8,
+                            children=[
+                              html.P(),
+                              dcc.Graph(id='planned_downtime', config={'displayModeBar': False}),
+                            ]),
+                         dbc.Col(width=4,
+                            children=[
+                              html.P(),
+                              dcc.Graph(id='planned_downtime_piechart', config={'displayModeBar': False}),
+                            ])
+                       ]),
+                      # style={"background-color": "#ABBAEA"},
+                      ), 
+                      #############################################################
+                      html.Div(
+                        dbc.Row([
+                      
+                          dbc.Col(width=8,
+                            children=[
+                              html.P(),
+                              dcc.Graph(id='fig_ktg_3y_by_months_id', config={'displayModeBar': False}),
+                            ]),
+                         
+                       ]),
+                      # style={"background-color": "#ABBAEA"},
+                      ), 
+
+
+
+                      #############################################################
+                       
+                       html.Hr(),
+                       html.P(),
+                       dcc.Graph(id='ktg_by_years', config={'displayModeBar': False}),
+                       html.Hr(),
+                       html.Div(
+                        dbc.Row([
+                      
+                          dbc.Col(width=12,
+                            children=[
+                              html.Div([
+                                html.P(),
+                                
+                                html.P(),
+                                dbc.Button("Выгрузить ктг.xlsx", id="btn_download_ktg_table", size="sm",
+                                           style={'marginBottom': '3px',
+                                                  'marginTop': '3px',
+                                                  'backgroundColor': '#232632'}, ),
+                                dcc.Download(id="download_ktg_table")
+                        ]),
+                              
+                            ]),
+                         
+                       ]),
+                      # style={"background-color": "#ABBAEA"},
+                      ),
+                       html.Div(id='ktg_by_month_table'), 
+
+
+
+
+                      
+                      # html.P(),
+                       # dcc.Graph(id='ktg_by_month', config={'displayModeBar': False}),
+                       # html.Hr(),
+                       # html.P(),
+                       # dcc.Graph(id='ktg_by_weeks', config={'displayModeBar': False}),
+                    ]),
+                
+            ]),
+            )
+            #dbc.Row([
+                
+            #]),
+        
+       
+          ]
+    )
+    return maintanance_chart_tab_block
